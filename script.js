32977f304d4bbe6dfe2286047ff92c6925676e7d
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-05-05 22:37:55 -0400

    html is completed, and the id's were changed to match the 24hr format, so that the function can compare it properly. Js file is completed, there was a mistake in one of the variables and it was fixed. Updated the timeinterval so it updates every hour

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 const localeSettings = {};
   dayjs.locale(localeSettings);
 
@@ -8,20 +6,24 @@ $(function () {
 
   $(document).ready(function() {
     function displayCurrentDate() {
-      var currentDate = dayjs().format('dddd, MMMM YYYY');
+      var currentDate = dayjs().format('dddd, MMMM DD YYYY');
       $('#currentDay').text(currentDate);
-      const timeElement = $('#time');
-      const currentTime = dayjs().format('hh:mm:ss A');
-      timeElement.text(currentTime);
+      const currentHour= dayjs().hour();
+      $('#time').text(dayjs().format('hh:mm:ss A'));
+      return currentHour;
     }
     displayCurrentDate();
-    
-}
-
-)
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
+  
+  function colorByHour() {
+    const currentHour = displayCurrentDate()
+    $('.time-block').each(function() {
+      const blockHour = parseInt(this.id);
+      $(this).toggleClass('past', blockHour < currentHour);
+      $(this).toggleClass('future', blockHour > currentHour);
+      $(this).toggleClass('present', blockHour === currentHour);
+      
+    });
+  }
 
   function textEntry() {
     $('.saveBtn').on('click', function() {
@@ -30,26 +32,10 @@ $(function () {
       localStorage.setItem(key, value);
     });
   }
-  
- 
-
-
-
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-
-  function colorByHour() {
-    $('.time-block').each(function() {
-      const blockHour = parseInt(this.id);
-      $(this).toggleClass('past', blockHour < currentHour);
-      $(this).toggleClass('present', blockHour === currentHour);
-      $(this).toggleClass('future', blockHour > currentHour);
-    });
-  }
 
   function changeColor() {
+    const currentHour = dayjs().hour();
+    $('.time-block').removeClass('past present future');
     $('.time-block').each(function() {
       const blockHour = parseInt(this.id);
       if (blockHour == currentHour) {
@@ -62,33 +48,14 @@ $(function () {
     });
   }
 
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-  
- 
- 
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
   $('.time-block').each(function() {
     const key = $(this).attr('id');
     const value = localStorage.getItem(key);
     $(this).children('.description').val(value);
   });
 
-  // TODO: Add code to display the current date in the header of the page.
-  
-
-
   textEntry();
   changeColor();
-colorByHour();
-setInterval(updateTime, 1000)
+setInterval(colorByHour,60000);
+});
 });
